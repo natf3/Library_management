d8b23a567b0e7b59d09034489d659cc2d9654b66
Natalia Frankiewicz <n.frankiewicz@gmail.com>
2021-09-26 01:42:34 +0200

    added getters and setters

--- src/main/java/com/example/library/user/User.java
+++ src/main/java/com/example/library/user/User.java
@@ -29,8 +29,35 @@ public class User {
     public User() {
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getEmail() {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
 }
--- src/main/java/com/example/library/user/UserController.java
+++ src/main/java/com/example/library/user/UserController.java
@@ -19,7 +19,7 @@ public class UserController {
         return userService.getUsers();
     }
 
-    @PostMapping
+    @PostMapping(path = "register")
     public void registerNewUser(@RequestBody User user){
         userService.addNewUser(user);
     }
